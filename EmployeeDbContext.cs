c20f0d74aa52218123003ea41a78c9ca54d06f19
Dipakshi Saxena <75835612+Dipakshi26@users.noreply.github.com>
2022-07-01 16:39:16 +0530

    Add files via upload

---
+++ EmployeeDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Employee.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<EmployeeClass> Employees { get; set; }
+        public EmployeeDbContext()
+        {
+
+        }
+        public EmployeeDbContext(DbContextOptions options)
+       : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-3AOH9UNJ\SQLEXPRESS;Database=WebApiDB;Trusted_Connection=True");
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Employee.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<EmployeeDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CrudWebApiDbConnection") ??
+    throw new InvalidOperationException("Connection string 'CrudWebApiDbConnection' not found.")));
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
